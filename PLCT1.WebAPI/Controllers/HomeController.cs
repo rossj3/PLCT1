cc5f70e5b0291e141c9ab54c125a4977ec178cdc
rossj3 <rossj3@gmail.com>
2014-05-10 18:18:37 -0500

    Added about/contact pages.

--- PLCT1.WebAPI/Controllers/HomeController.cs
+++ PLCT1.WebAPI/Controllers/HomeController.cs
@@ -12,5 +12,15 @@ namespace PLCT1.WebAPI.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
